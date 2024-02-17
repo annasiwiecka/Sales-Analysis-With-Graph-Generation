efb673ca8c4b13f7df5b6bb1eef74c72afc60fb8
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2024-02-17 22:16:24 +0100

    Position, Sale, CSV model

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils import timezone
+
+from products.models import Product
+from customers.models import Customer
+from profiles.models import Profile
 
 # Create your models here.
+
+
+class Position(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    created_at = models.DateTimeField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price * self.quantity
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"id: {self.id}, product: {self.product.name}, category: {self.product.category} quantity: {self.quantity}"
+
+
+class Sale(models.Model):
+    transactions_id = models.CharField(max_length=12, blank=True)
+    positions = models.ManyToManyField(Position)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    salesman = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def sav(self, *args, **kwargs):
+        if self.transactions_id == "":
+            self.transactions_id == ""
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def get_positions(self):
+        return self.positions.all()
+
+    def __str__(self):
+        return f"Sales for tge amount of ${self.total_price}"
+
+
+class CSV(models.Model):
+    file_name = models.FileField(upload_to="csvs")
+    activated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.file_name
