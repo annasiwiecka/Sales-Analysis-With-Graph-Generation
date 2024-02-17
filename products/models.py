7d0eb4cf3258a579ff7c6926a4ca924485871ea5
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2024-02-17 02:33:01 +0100

    Category and Product Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='products', default='no_picture.jpg')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} {self.category} {self.created_at.strftime('%d/%m/%Y')}'
