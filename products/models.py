88d2567138c478a686d7543228513637e606df7e
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2024-02-17 02:40:36 +0100

    edit: Category Model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='products', default='no_picture.jpg')
