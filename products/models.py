ffb4efd166285e1e54fa2e38815bb1c4be71650b
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2024-02-17 22:26:02 +0100

    Deleting category model

--- products/models.py
+++ products/models.py
@@ -2,16 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='products', default='no_picture.jpg')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
