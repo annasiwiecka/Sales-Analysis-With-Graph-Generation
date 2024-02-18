81eb1d35e483cdb0f762767aa644fdd06f2fb571
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2024-02-18 02:49:13 +0100

    add choices in home view

--- sales/views.py
+++ sales/views.py
@@ -7,6 +7,10 @@ from .forms import SalesSearchForm
 
 def home(request):
     form = SalesSearchForm(request.POST or None)
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
 
     return render(request, 'sales/home.html', {
         'form': form
