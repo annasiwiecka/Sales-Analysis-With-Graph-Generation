9091606fd2e443f81d7949593a57d1eaeea367a5
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2024-02-24 11:55:27 +0100

    profile page

--- profiles/models.py
+++ profiles/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default='no bio')
-    avatar = models.ImageField(upload_to='avatars', default='no_picture.jpg')
+    avatar = models.ImageField(upload_to='avatars', default='default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.username
+
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
+
+def my_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST or None, request.FILES or None, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Your profile has been updated")
+    else:
+        form = ProfileForm(instance=profile)
+    
+    return render(request, 'profiles/main.html', {
+        'profile': profile,
+        'form': form,
+    })
